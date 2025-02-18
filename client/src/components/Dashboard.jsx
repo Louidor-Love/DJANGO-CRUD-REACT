bf45968817d488639d17f3d9389ab8f183e2ac62
louidor-love <louidorlove@gmail.com>
2025-02-18 19:28:31 -0300

    nuevo

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,16 +1,12 @@
+import { useEffect, useState } from "react";
+
+
+
 export function Dashboard() {
     return (
       <div>
-        <form action="">  
-          <label>Mail</label>
-          <input type="text" name="username" value="" id="username" className="form-control" 
-              placeholder="username"
-          />
-          <label>Contraseña</label>
-          <input type="text" name="username" value="" id="username" className="form-control" 
-              placeholder="username"
-          />
-        </form>
+        Dashboard
+
       </div>
     );
   }
--- client/src/pages/UserFormPage.jsx
+++ client/src/pages/UserFormPage.jsx
@@ -1,7 +1,27 @@
 import { toast } from "react-hot-toast";
+import { useEffect, useState } from "react";
 
 export function UserformPage(){
+    useEffect(() => {
+        console.log("pagina cargada");
+    }, []);
     return (
-        <div>Create User</div>
+        <div>
+        <form action="">  
+          <label>Mail</label>
+          <input type="text" name="username" value="" id="username" className="form-control" 
+              placeholder="username"
+          />
+          <label>Contraseña</label>
+          <input type="text" name="username" value="" id="username" className="form-control" 
+              placeholder="username"
+          />
+          <label>Confirma Contraseña</label>
+          <input type="text" name="username" value="" id="username" className="form-control" 
+              placeholder="username"
+          />
+          <button>login</button>
+        </form>
+      </div>
     )
 }
