143759e501a9192a1e10177e97799972bf4048e8
louidor-love <louidorlove@gmail.com>
2025-02-25 13:50:08 -0300

    user

--- user/serializers.py
+++ user/serializers.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = get_user_model
+        model = get_user_model()
         fields = ['email','password','name']
         extra_kwargs = {
             "password": {"write_only": True},  # No se devuelve en la respuesta
